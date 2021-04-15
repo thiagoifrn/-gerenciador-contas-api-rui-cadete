6c70010000d83e91527e272c06bc01442cc4609f
thiagoifrn <thiagoifrn@gmail.com>
2021-04-15 13:21:34 -0300

    Adicionando @crossOrigin para configurar o CORS

--- src/main/java/br/com/developer/gerenciador/controller/ContaController.java
+++ src/main/java/br/com/developer/gerenciador/controller/ContaController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.com.developer.gerenciador.model.Conta;
 import br.com.developer.gerenciador.repository.ContaRepository;
 import br.com.developer.gerenciador.service.ContaService;
 @RestController
+@CrossOrigin(originPatterns = "*")
 @RequestMapping("/contas")
 public class ContaController {
 
